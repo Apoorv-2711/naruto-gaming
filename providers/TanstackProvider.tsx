3d41b5650d4b77fb58039cd19adde05ce1492c6a
Apoorv-2711 <apoorvjain162@gmail.com>
2024-01-03 23:36:45 +0530

    sone fixes

--- providers/TanstackProvider.tsx
+++ providers/TanstackProvider.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 const TanstackProvider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 };
 
