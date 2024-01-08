2a784a75437da1e6db10e9b47bcc81912cacd6dd
codefreak69 <iamvikashkumar30@gmail.com>
2024-01-08 17:00:59 +0530

    FIX : Build Error Fixed  ^_^

--- app/api/home/route.ts
+++ app/api/home/route.ts
@@ -4,15 +4,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { ScrapedHomePage } from "@/models/parsers/homePage";
 
 export async function GET(
-  req: NextApiRequest,
-  res: NextApiResponse<ReturnType<typeof scrapeHomePage>>
+  req: NextRequest,
+  res: NextResponse<ReturnType<typeof scrapeHomePage>>
 ) {
   try {
     const data = await scrapeHomePage();
 
     console.log(data, "Data from api..");
     return new Response(JSON.stringify(data), {
-      headers: { "content-type": "application/json", },
+      headers: { "content-type": "application/json" },
     });
   } catch (err: any) {
     console.error(err);
