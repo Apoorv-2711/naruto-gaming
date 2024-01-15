909b6c0a7e37083f1bc437d96e7a5b072f6df7cb
Apoorv-2711 <apoorvjain162@gmail.com>
2024-01-16 03:45:48 +0530

    Feat: layout fix

--- components/LatestEpisodes.tsx
+++ components/LatestEpisodes.tsx
@@ -21,35 +21,43 @@ const LatestEpisodes: FC<LatestEpisodesProps> = ({ latestEpisodeData }) => {
             className="group mb-5 w-full space-y-3 after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
             key={data.id}
           >
-            <HoverCard openDelay={0} closeDelay={0} >
-                <HoverCardTrigger asChild>
-            <Image
-              alt={data.name}
-              className="transform rounded-lg brightness-90 transition will-change-auto group-hover:blur-sm group-hover:brightness-100"
-              style={{ transform: "translate3d(0, 0, 0)", 
-              aspectRatio: "3/4", objectFit: "cover", objectPosition: "center"
-             }}
-              src={data.poster}
-              width={720}
-              height={480}
-              sizes="(max-width: 640px) 100vw,
+            <HoverCard openDelay={0} closeDelay={0}>
+              <HoverCardTrigger asChild>
+                <Image
+                  alt={data.name}
+                  className="transform rounded-lg brightness-90 transition will-change-auto hover:blur-sm group-hover:brightness-100"
+                  style={{
+                    transform: "translate3d(0, 0, 0)",
+                    aspectRatio: "3/4",
+                    objectFit: "cover",
+                    objectPosition: "center",
+                  }}
+                  src={data.poster}
+                  width={720}
+                  height={480}
+                  sizes="(max-width: 640px) 100vw,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 33vw,
                   25vw"
-            />
-            </HoverCardTrigger>
-            <HoverCardContent className=" dark min-w-[350px] w-min  bg-[#25242489] text-white rounded-lg border-none bg-opacity-60 backdrop-blur-lg p-0 m-0">
-                <InsideHoverCard id={data?.id}/>
-                </HoverCardContent>
-              </HoverCard>
-            <div className="flex flex-col space-y-2 items-start text-white whitespace-nowrap">
-              <h3 className="text-sm font-bold">
-                {data.name.slice(0, 20)}
-                {data.name.length > 20 ? "..." : ""}
-              </h3>
-              <p className="text-sm text-gray-300">{data.type}
-              <Dot size={25} className="inline-block" />
-              {data.duration}
+                />
+              </HoverCardTrigger>
+              <HoverCardContent className=" dark min-w-[350px] w-min  bg-[#25242489] text-white rounded-lg border-none bg-opacity-60 backdrop-blur-lg p-0 m-0">
+                <InsideHoverCard id={data?.id} />
+              </HoverCardContent>
+            </HoverCard>
+            <div className="flex flex-col space-y-2 items-start text-white overflow-hidden">
+              <div className=" flex flex-row hover:text-[#f97316] cursor-pointer" style={{
+                maxWidth: "100%",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+              }}>
+                <h3 className="text-sm font-bold">{data.name}</h3>
+              </div>
+              <p className="text-sm text-gray-300">
+                {data.type}
+                <Dot size={25} className="inline-block" />
+                {data.duration}
               </p>
             </div>
           </div>
