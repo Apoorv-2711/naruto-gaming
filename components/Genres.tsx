c554d1914ea04fee3d908ab020e42e9a28d69aa3
Apoorv-2711 <apoorvjain162@gmail.com>
2024-01-16 03:26:16 +0530

    Feat: Some UI fix

--- components/Genres.tsx
+++ components/Genres.tsx
@@ -9,47 +9,47 @@ type GenresProps = {
 
 const Genres: React.FC<GenresProps> = ({ genresData }) => {
   const [show, setShow] = React.useState(false);
-  const [data, setData] = React.useState<home["genres"]>(genresData.slice(0, 24));
+  const [data, setData] = React.useState<home["genres"]>(
+    genresData.slice(0, 24)
+  );
 
   const colors = React.useMemo(() => {
     return genresData.map((genre) => getRandomColor());
   }, [genresData]);
-  
+
   useEffect(() => {
     if (show) {
       setData(genresData);
     } else {
       setData(genresData.slice(0, 24));
     }
-  } , [show, genresData]);
+  }, [show, genresData]);
 
   return (
     <div className="xl:w-1/4 w-full px-4 h-fit">
       <h3 className="text-2xl  font-bold mt-12 mb-4 text-red-500">Genres</h3>
       <div className=" bg-[#252424]  p-4">
-        <div className="flex flex-wrap gap-3 justify-start ">
+        <div className="columns-3 ">
           {data.map((genre) => (
             <h2
-              className="flex basis-1/4 flex-row  text-white rounded-md  mb-2 mx-3 whitespace-nowrap"
+              className="  text-white rounded-md w-full hover:bg-white/10 p-3 truncate"
+              style={{ color: colors[genresData.indexOf(genre)],
+               }}
               key={genre}
             >
-              <span className="text-sm font-semibold"
-              style={{color: colors[genresData.indexOf(genre)]}}
-              >
-                {genre}
-              </span>
+              <span className="text-sm font-semibold">{genre}</span>
             </h2>
           ))}
         </div>
-          <Button
-            className="flex flex-row text-white rounded-md w-full mt-2"
-            onClick={() => setShow(!show)}
-            variant={"outline"}
-          >
-            <span className="text-sm font-semibold">
-              {show ? "Show Less" : "Show More"}
-            </span>
-          </Button>
+        <Button
+          className="flex flex-row text-white rounded-md w-full mt-2"
+          onClick={() => setShow(!show)}
+          variant={"outline"}
+        >
+          <span className="text-sm font-semibold">
+            {show ? "Show Less" : "Show More"}
+          </span>
+        </Button>
       </div>
     </div>
   );
@@ -57,12 +57,11 @@ const Genres: React.FC<GenresProps> = ({ genresData }) => {
 
 export default Genres;
 
-
 function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
+  const letters = "0123456789ABCDEF";
+  let color = "#";
   for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
