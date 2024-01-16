22a3f146eb3082fc30f4f76c8ea76a5d3f809a4f
codefreak69 <iamvikashkumar30@gmail.com>
2024-01-16 19:20:23 +0530

    Experimetal: Typewriter effect

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,23 @@
 import Particles from "@/components/Particles";
 import { Button } from "@/components/ui/button";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import Link from "next/link";
 import React from "react";
 
 const navigation = [{ name: "Go to Home Page", href: "/home" }];
+
+const words = [
+  {
+    text: "Naruto",
+    className:
+      "z-10 text-4xl duration-1000 text-white cursor-default  animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap ",
+  },
+  {
+    text: "Gaming",
+    className:
+      "z-10 text-4xl duration-1000 text-white cursor-default  animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap ",
+  },
+];
 
 const LandingPage = () => {
   return (
@@ -11,32 +25,35 @@ const LandingPage = () => {
       <nav className="my-16 animate-fade-in z-10">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className=""
-            >
-              <Button variant={"link"} className="text-sm duration-500 text-zinc-500 hover:text-zinc-300 p-0">{item.name}</Button>
+            <Link key={item.href} href={item.href} className="">
+              <Button
+                variant={"link"}
+                className="text-sm duration-500 text-zinc-500 hover:text-zinc-300 p-0"
+              >
+                {item.name}
+              </Button>
             </Link>
           ))}
         </ul>
       </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles className="absolute inset-0 animate-fade-in" quantity={1000} />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Naruto GaminG
-      </h1>
+
+      <TypewriterEffectSmooth words={words} />
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in z-10">
         <h2 className="text-sm text-zinc-500 ">
-          We are building a anime streaming website called Naruto Gaming. It&apos;s
-          still in development, and We are making progress on it every day. If you want to contribute, please contact us on{" "}
-          <Link
-            href="https://twitter.com/Apoorv2711"
-            target="_blank"
-          >
-            <Button variant={"link"} className="text-zinc-300 animate-fade-in hover:text-zinc-200 p-0">Twitter</Button>
+          We are building a anime streaming website called Naruto Gaming.
+          It&apos;s still in development, and We are making progress on it every
+          day. If you want to contribute, please contact us on{" "}
+          <Link href="https://twitter.com/Apoorv2711" target="_blank">
+            <Button
+              variant={"link"}
+              className="text-zinc-300 animate-fade-in hover:text-zinc-200 p-0"
+            >
+              Twitter
+            </Button>
           </Link>
         </h2>
         <h2 className="text-sm animate-fade-in text-zinc-500 absolute bottom-10 left-0 right-0 mx-auto ">
@@ -46,16 +63,25 @@ const LandingPage = () => {
             className=""
             target="_blank"
           >
-            <Button variant={"link"} className="text-zinc-300 hover:text-zinc-200 p-0">Apoorv Jain</Button>
-          </Link>
-          {" "}
-            &amp;{" "}
+            <Button
+              variant={"link"}
+              className="text-zinc-300 hover:text-zinc-200 p-0"
+            >
+              Apoorv Jain
+            </Button>
+          </Link>{" "}
+          &amp;{" "}
           <Link
             href="https://github.com/Codefreak69"
             className="text-zinc-300 hover:text-zinc-200"
             target="_blank"
           >
-            <Button variant={"link"} className="text-zinc-300 hover:text-zinc-200 p-0">Vikash Kumar</Button>
+            <Button
+              variant={"link"}
+              className="text-zinc-300 hover:text-zinc-200 p-0"
+            >
+              Vikash Kumar
+            </Button>
           </Link>
         </h2>
       </div>
